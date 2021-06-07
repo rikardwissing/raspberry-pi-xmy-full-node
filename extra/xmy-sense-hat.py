8e6d7a59737e9f3cf6336f4baed59acbc10a71cb
rikardwissing <rikard.wissing@teamtailor.com>
2021-06-07 20:57:55 +0200

    Update xmy-sense-hat.py

--- extra/xmy-sense-hat.py
+++ extra/xmy-sense-hat.py
@@ -3,11 +3,17 @@
 from sense_hat import SenseHat
 from time import sleep
 import subprocess
+import json
 
 sense = SenseHat()
-sense.load_image(
-    "/home/pi/raspberry-pi-xmy-full-node-main/extra/logo_refresh_8x8.png")
-sense.low_light = True
+
+algo_colors = [(246, 108, 190), (253, 149, 30), (253, 239, 65),
+               (113, 221, 150), (131, 233, 237), (134, 183, 240), (175, 72, 218)]
+
+
+def load_logo():
+    sense.load_image(
+        "/home/pi/raspberry-pi-xmy-full-node-main/extra/logo_refresh_8x8.png")
 
 
 def rotate_image(rot_diff):
@@ -17,6 +23,15 @@ def rotate_image(rot_diff):
         sense.set_rotation((i*90+rot_diff) % 360)
         sleep(0.1)
 
+
+def algo_flash(algo_id):
+    sense.clear(algo_colors[algo_id])
+    sleep(3)
+    load_logo()
+
+
+sense.low_light = True
+load_logo()
 
 block_count = 0
 prev_block_count = 0
@@ -46,14 +61,26 @@ while True:
 
     try:
         block_count = subprocess.check_output(
-            ['/home/pi/myriadcoin/bin/myriadcoin-cli', '-rpcpassword=rpc', 'getblockcount'])
+            ['/home/pi/myriadcoin/bin/myriadcoin-cli', '-rpcpassword=rpc', 'getblockcount']).strip()
         print(block_count)
 
         if block_count != prev_block_count:
             prev_block_count = block_count
+
+            block_hash = subprocess.check_output(
+                ['/home/pi/myriadcoin/bin/myriadcoin-cli', '-rpcpassword=rpc', 'getbestblockhash']).strip()
+            block_info = subprocess.check_output(
+                ['/home/pi/myriadcoin/bin/myriadcoin-cli', '-rpcpassword=rpc', 'getblock', block_hash]).strip()
+            block_info = json.loads(block_info)
+
+            print(block_info['pow_algo'])
+
             sense.low_light = False
+
             rotate_image(rotation)
+            algo_flash(block_info['pow_algo_id'])
             sleep(3)
+
             sense.low_light = True
 
     except:
